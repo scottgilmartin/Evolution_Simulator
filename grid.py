8dea50e3361ed499740108078305683306f0eb3e
scottgilmartin <41211277+scottgilmartin@users.noreply.github.com>
2019-02-03 16:34:58 +0000

    Update grid.py

--- grid.py
+++ grid.py
@@ -12,15 +12,17 @@ class Grid:
         Initialize grid size, create img_grid and dna_grid lists.
         """
         self.grid_size = grid_size  
-        self.img_grid = []  #a list of the individual avatar matrices which are stitched together to create the avatar layer of grid (img_grid_mat)
-        self.dna_grid = []  #a list of the individual dna matrices from which the corresponding avatars above are drawn, used to create the dna layer of the grid (dna_grid_mat)
+        self.img_grid = []  # a list of the individual avatar matrices
+        # which are stitched together to create the avatar layer of grid (img_grid_mat)
+        self.dna_grid = []  # a list of the individual dna matrices from which
+        # the corresponding avatars above are drawn, used to create the dna layer of the grid (dna_grid_mat)
 
     def add_sprite(self, sprite): 
         """
         Add sprite objects to the grid.
         """
-        self.img_grid.append(sprite.draw()) #adding the drawn avatars
-        self.dna_grid.append(sprite.dna) #adding the corresponding dna from which the above avatars are drawn
+        self.img_grid.append(sprite.draw())  # adding the drawn avatars
+        self.dna_grid.append(sprite.dna)  # adding the corresponding dna from which the above avatars are drawn
         return self.img_grid, self.dna_grid
 
     def construct_grid(self):
@@ -38,7 +40,8 @@ class Grid:
                     for y in range(j * SPRITE_HEIGHT, (j + 1) * SPRITE_HEIGHT):
                         img_grid_mat[x, y] += self.img_grid[self.grid_size * j + i][x - i * SPRITE_WIDTH, y - j * SPRITE_HEIGHT] 
                         dna_grid_mat[x, y] += self.dna_grid[self.grid_size * j + i][x - i * SPRITE_WIDTH, y - j * SPRITE_HEIGHT]
-                        #stitch the individual sprites together by drawing them in the specified position in the grid matrices
+                        # stitch the individual sprites together
+                        # by drawing them in the specified position in the grid matrices
         return img_grid_mat, dna_grid_mat
         
 
